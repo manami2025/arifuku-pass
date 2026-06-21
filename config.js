e93ca686f949f38adb029a2bd911e871d6ef55e8
manami <manami.fujita@flat-it.jp>
2026-06-21 18:27:54 +0900

    6/21 18:27

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // config.js
 const SUPABASE_URL = 'https://udxltthbwkpyaeyzsuzm.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVkeGx0dGhid2tweWFleXpzdXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NjIyMzAsImV4cCI6MjA5NzQzODIzMH0.aUBBQc5pQvGCkNU9JHPonpLJLfmtbdqym11Abt9ovn8'
+const LIFF_ID = '2010451666-KY86SM9C'
 
-export { SUPABASE_URL, SUPABASE_ANON_KEY }
+export { SUPABASE_URL, SUPABASE_ANON_KEY, LIFF_ID }
